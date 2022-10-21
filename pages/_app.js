64535e5e3e849c383cd1c7d724411327c0a27f68
Amy Eddins <amyleigheddins@gmail.com>
2022-10-20 19:29:07 -0500

    Fix mobile view

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=0.4"/>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
